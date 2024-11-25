6ba1261ad6a7120b33c96d1271b60960cab21136
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-11-25 14:25:47 +0000

    - dimensions of matrix are available from 2D array

--- Program.cs
+++ Program.cs
@@ -14,10 +14,10 @@ public static class Program
     var WindowSize = new Vector2(1200, 800);
     var maxPoints = new Vector2(WindowSize.X / CellSize + 1, WindowSize.Y / CellSize + 1);
 
-    var points = new Point[(int) maxPoints.X, (int) maxPoints.Y];
-    for (var x = 0; x < maxPoints.X; x++)
+    var points = new Point[(int) (WindowSize.X / CellSize + 1), (int) (WindowSize.Y / CellSize + 1)];
+    for (var x = 0; x < points.GetLength(0); x++)
     {
-      for (var y = 0; y < maxPoints.Y; y++)
+      for (var y = 0; y < points.GetLength(1); y++)
       {
         points[x, y] = new Point(x * CellSize, y * CellSize, rng.CoinFlip());
       }
